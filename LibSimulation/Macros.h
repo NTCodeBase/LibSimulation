6a9b27e58362c6a6f07de4c0079d76621c8987d6
Nghia Truong <nghiatruong.vn@gmail.com>
2019-01-02 09:19:04 -0700

    add macro

--- LibSimulation/Macros.h
+++ LibSimulation/Macros.h
@@ -15,15 +15,19 @@
 #pragma once
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#define __NT_DECLARE_PARTICLE_SOLVER_ACCESSORS                                                                                             \
-    ParameterManager & paramManager() { return this->m_ParameterManager; }                                                                 \
-    Parameter& globalParams(const char* paramName) { return paramManager().parameter("GlobalParameters", paramName); }                     \
-    Parameter& simParams(const char* paramName) { return paramManager().parameter("SimulationParameters", paramName); }                    \
-    PropertyManager& propertyManager() { return this->m_PropertyManager; }                                                                 \
-    Logger& logger() { assert(this->m_Logger != nullptr); return *this->m_Logger; }                                                        \
-                                                                                                                                           \
-    const ParameterManager& paramManager() const { return this->m_ParameterManager; }                                                      \
-    const Parameter&        globalParams(const char* paramName) const { return paramManager().parameter("GlobalParameters", paramName); }  \
-    const Parameter&        simParams(const char* paramName) const { return paramManager().parameter("SimulationParameters", paramName); } \
-    const PropertyManager&  propertyManager() const { return this->m_PropertyManager; }                                                    \
-    const Logger&           logger() const { assert(this->m_Logger != nullptr); return *this->m_Logger; }
+#define __NT_DECLARE_PARTICLE_SOLVER_ACCESSORS                                                                                            \
+    ParameterManager & paramManager() { return this->m_ParameterManager; }                                                                \
+    PropertyGroup& boundaryParticleData() { return this->m_PropertyManager.group("BoundaryParticle"); }                                   \
+                                                                                                                                          \
+    Parameter& globalParams(const char* paramName) { return paramManager().parameter("GlobalParameters", paramName); }                    \
+    Parameter& simParams(const char* paramName) { return paramManager().parameter("SimulationParameters", paramName); }                   \
+    PropertyManager& propertyManager() { return this->m_PropertyManager; }                                                                \
+    Logger& logger() { assert(this->m_Logger != nullptr); return *this->m_Logger; }                                                       \
+                                                                                                                                          \
+    const ParameterManager& paramManager() const { return this->m_ParameterManager; }                                                     \
+    const PropertyGroup&    boundaryParticleData() const { return this->m_PropertyManager.group("BoundaryParticle"); }                    \
+                                                                                                                                          \
+    const Parameter&       globalParams(const char* paramName) const { return paramManager().parameter("GlobalParameters", paramName); }  \
+    const Parameter&       simParams(const char* paramName) const { return paramManager().parameter("SimulationParameters", paramName); } \
+    const PropertyManager& propertyManager() const { return this->m_PropertyManager; }                                                    \
+    const Logger&          logger() const { assert(this->m_Logger != nullptr); return *this->m_Logger; }
