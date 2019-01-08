261365ada16bc13f250536c16b6c63a49fc8ac4d
Nghia Truong <nghiatruong.vn@gmail.com>
2019-01-07 17:38:23 -0700

    fix macro

--- LibSimulation/Macros.h
+++ LibSimulation/Macros.h
@@ -15,19 +15,8 @@
 #pragma once
 
 //-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#define __NT_DECLARE_PARTICLE_SOLVER_ACCESSORS                                                                                            \
-    ParameterManager & paramManager() { return this->m_ParameterManager; }                                                                \
-    PropertyGroup& boundaryParticleData() { return this->m_PropertyManager.group("BoundaryParticle"); }                                   \
-                                                                                                                                          \
-    Parameter& globalParams(const char* paramName) { return paramManager().parameter("GlobalParameters", paramName); }                    \
-    Parameter& simParams(const char* paramName) { return paramManager().parameter("SimulationParameters", paramName); }                   \
-    PropertyManager& propertyManager() { return this->m_PropertyManager; }                                                                \
-    Logger& logger() { assert(this->m_Logger != nullptr); return *this->m_Logger; }                                                       \
-                                                                                                                                          \
-    const ParameterManager& paramManager() const { return this->m_ParameterManager; }                                                     \
-    const PropertyGroup&    boundaryParticleData() const { return this->m_PropertyManager.group("BoundaryParticle"); }                    \
-                                                                                                                                          \
-    const Parameter&       globalParams(const char* paramName) const { return paramManager().parameter("GlobalParameters", paramName); }  \
-    const Parameter&       simParams(const char* paramName) const { return paramManager().parameter("SimulationParameters", paramName); } \
-    const PropertyManager& propertyManager() const { return this->m_PropertyManager; }                                                    \
-    const Logger&          logger() const { assert(this->m_Logger != nullptr); return *this->m_Logger; }
+#define __NT_DECLARE_PARTICLE_SOLVER_ACCESSORS                                                \
+    GlobalParameters<Real_t>&globalParams() { return this->m_GlobalParameters; }              \
+    Logger& logger() { assert(this->m_Logger != nullptr); return *this->m_Logger; }           \
+    const GlobalParameters<Real_t>& globalParams() const { return this->m_GlobalParameters; } \
+    const Logger& logger() const { assert(this->m_Logger != nullptr); return *this->m_Logger; }
